30c5d8ea182f29b74d7183964c65de5c9a8a06b5
Manning Stinson <manning@manningstinson.com>
2023-12-09 07:49:57 -0800

    updated monty.c

--- monty.c
+++ monty.c
@@ -1,29 +1,40 @@
-// monty.c
+#include "monty.h"  // Include your header file
 
-#include "monty.h"
-#include <stdio.h>
-
-int main(int argc, char *argv[])
-{
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        return EXIT_FAILURE;
+int main(int argc, char *argv[]) {
+    /* Check for the correct number of command-line arguments */
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
 
+    /* Open the file for reading */
     FILE *file = fopen(argv[1], "r");
-    if (!file)
-    {
+    if (!file) {
         fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        return EXIT_FAILURE;
+        exit(EXIT_FAILURE);
     }
 
-    // Placeholder for your file parsing logic
-    // Replace this with your actual implementation
-    unsigned int line_number = 4;  // Simulating an error on line 4
-    fprintf(stderr, "L%u: usage: push integer\n", line_number);
+    /* Read each line from the file */
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    unsigned int line_number = 1;
 
+    while ((read = getline(&line, &len, file)) != -1) {
+        /* Remove the newline character if present */
+        if (line[read - 1] == '\n') {
+            line[read - 1] = '\0';
+            read--;
+        }
+
+        /* Process the line (use your existing code for this part) */
+        /* ... */
+    }
+
+    /* Clean up resources */
+    free(line);
     fclose(file);
+
     return EXIT_SUCCESS;
 }
 
