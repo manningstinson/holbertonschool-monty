ace089c6e5bea5e42effe5219102856e08c213b9
Manning Stinson <manning@manningstinson.com>
2023-12-09 12:48:13 -0800

    updated pint

--- monty.h
+++ monty.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>  // Include this for strcmp
 
 /* Define stack structure if not already done in monty.h */
 typedef struct stack {
@@ -13,6 +14,10 @@ typedef struct stack {
 /* Function prototypes */
 void push(stack_t **stack, int value);
 void pall(stack_t **stack);
+void pint(stack_t **stack);
+void pop(stack_t **stack);
+void swap(stack_t **stack);
+void add(stack_t **stack);
 int read_and_execute_instructions(FILE *file);
 
 #endif /* MONTY_H */
