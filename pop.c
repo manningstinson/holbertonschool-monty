76fdbfe45de3dd56c901e0b966e2d20e989e66da
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-09 16:56:50 -0600

    Update pop.c

--- pop.c
+++ pop.c
@@ -4,8 +4,6 @@ void pop(stack_t **stack) {
     if (*stack) {
         stack_t *temp = *stack;
         *stack = (*stack)->next;
-        if (*stack)
-            (*stack)->prev = NULL;
         free(temp);
     } else {
         fprintf(stderr, "L%zu: can't pop an empty stack\n", line_number);
