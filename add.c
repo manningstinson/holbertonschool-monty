786e9d8067358ccdb6882a15cc2978d1e35ac319
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-09 17:26:21 -0600

    Update add.c

--- add.c
+++ add.c
@@ -1,9 +1,11 @@
 #include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 void add(stack_t **stack) {
     if (*stack && (*stack)->next) {
-        (*stack)->next->data += (*stack)->data;
-        pop(stack);  /* Remove the top element */
+        (*stack)->next->n += (*stack)->n;
+        pop(stack);
     } else {
         fprintf(stderr, "L%zu: can't add, stack too short\n", line_number);
         exit(EXIT_FAILURE);
