1ef8dd9850236d571429dfe3c06362ee4d9a2f6b
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-09 17:50:29 -0600

    Update swap.c

--- swap.c
+++ swap.c
@@ -2,7 +2,7 @@
 
 void swap(stack_t **stack) {
     if (*stack && (*stack)->next) {
-        int temp = (*stack)->data;
+        int temp = (*stack)->data;  // Change 'n' to 'data'
         (*stack)->data = (*stack)->next->data;
         (*stack)->next->data = temp;
     } else {
