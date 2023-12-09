f65c8bda5d599e2208caac204e642fd5c246e40d
Manning Stinson <manning@manningstinson.com>
2023-12-09 06:30:29 -0800

    updated pall.c and push.c

--- pall.c
+++ pall.c
@@ -3,6 +3,9 @@
 
 void pall(stack_t **stack, unsigned int line_number)
 {
+    (void)stack;  // To suppress the unused parameter warning
+    (void)line_number;  // To suppress the unused parameter warning
+
     /* Implementation of pall opcode */
 }
 
--- push.c
+++ push.c
@@ -1,8 +1,11 @@
-
+// push.c
 #include "monty.h"
 
 void push(stack_t **stack, int line_number, char *arg)
 {
+    (void)stack;  // To suppress the unused parameter warning
+    (void)line_number;  // To suppress the unused parameter warning
+
     /* Implementation of push opcode */
 }
 
