f98fd41f5dc9882f5a489153abd1287d54efd64f
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-09 17:41:22 -0600

    Update monty.h

--- monty.h
+++ monty.h
@@ -6,16 +6,22 @@
 
 extern size_t line_number;
 
+/**
+ * Data structure for a stack (or doubly linked list)
+ */
 typedef struct stack_s {
-    int n;
+    int data;
     struct stack_s *next;
 } stack_t;
 
+/* Function prototypes */
 void push(stack_t **stack, int value);
 void pall(stack_t **stack);
 void pint(stack_t **stack);
 void pop(stack_t **stack);
 void swap(stack_t **stack);
 void add(stack_t **stack);
+int read_and_execute_instructions(FILE *file);
+void free_stack(stack_t **stack);
 
 #endif /* MONTY_H */
