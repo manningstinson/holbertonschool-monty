c257f4a7ae89e3bd76f9ffbf8ff0182574c90959
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-09 16:59:21 -0600

    Update monty.h

--- monty.h
+++ monty.h
@@ -3,25 +3,34 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
-extern size_t line_number;
+#include <string.h>
 
 /**
- * Data structure for a stack (or doubly linked list)
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
  */
-typedef struct stack_s {
-    int data;
+typedef struct stack_s
+{
+    int n;
+    struct stack_s *prev;
     struct stack_s *next;
 } stack_t;
 
-/* Function prototypes */
+extern size_t line_number;
+
 void push(stack_t **stack, int value);
 void pall(stack_t **stack);
 void pint(stack_t **stack);
 void pop(stack_t **stack);
 void swap(stack_t **stack);
 void add(stack_t **stack);
-int read_and_execute_instructions(FILE *file);
+
 void free_stack(stack_t **stack);
+int read_and_execute_instructions(FILE *file);
 
 #endif /* MONTY_H */
