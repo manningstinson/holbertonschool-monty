88264aa269ca92b38c1b89b49dfb5e86cc39f630
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-09 16:57:48 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,23 +1,29 @@
 #include "monty.h"
+#include <stdio.h>
 
 int main(int argc, char *argv[]) {
+    /* Check for correct number of arguments */
     if (argc != 2) {
         fprintf(stderr, "USAGE: monty file\n");
         exit(EXIT_FAILURE);
     }
 
+    /* Open and read the Monty bytecode file */
     FILE *file = fopen(argv[1], "r");
     if (!file) {
         fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
         exit(EXIT_FAILURE);
     }
 
+    /* Read and execute instructions from the file */
     if (!read_and_execute_instructions(file)) {
         fprintf(stderr, "Error: Execution failed\n");
         fclose(file);
         exit(EXIT_FAILURE);
     }
 
+    /* Close the file and free allocated memory if needed */
     fclose(file);
+
     return EXIT_SUCCESS;
 }
