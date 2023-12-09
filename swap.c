e5e529c8014218540c76d6f67e284f132b90840d
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-09 17:26:05 -0600

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,10 +1,12 @@
 #include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 void swap(stack_t **stack) {
     if (*stack && (*stack)->next) {
-        int temp = (*stack)->data;
-        (*stack)->data = (*stack)->next->data;
-        (*stack)->next->data = temp;
+        int temp = (*stack)->n;
+        (*stack)->n = (*stack)->next->n;
+        (*stack)->next->n = temp;
     } else {
         fprintf(stderr, "L%zu: can't swap, stack too short\n", line_number);
         exit(EXIT_FAILURE);
