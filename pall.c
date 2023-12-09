b567a934a47512e05e55d85d2e8e66d2a71bb081
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-09 10:50:27 -0600

    Update pall.c

--- pall.c
+++ pall.c
@@ -1,13 +1,13 @@
+// pall.c
+
 #include "monty.h"
 
 void pall(stack_t **stack, unsigned int line_number) {
-    (void)line_number;  // Unused parameter
+    (void)line_number;
     stack_t *current = *stack;
 
-    // Iterate through the stack and print each element
     while (current) {
         printf("%d\n", current->n);
         current = current->next;
     }
 }
-
