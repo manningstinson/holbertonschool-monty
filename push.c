2adf3a836433622b7d13fab1c1c6d8e075486201
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-09 10:50:11 -0600

    Update push.c

--- push.c
+++ push.c
@@ -1,51 +1,46 @@
+// push.c
+
 #include "monty.h"
 
 void push(stack_t **stack, unsigned int line_number) {
     char *arg;
     int value;
 
-    // Check if the stack parameter is valid
     if (!stack) {
         fprintf(stderr, "L%u: stack not found\n", line_number);
         exit(EXIT_FAILURE);
     }
 
-    // Get the argument after push
     arg = strtok(NULL, " \t\n");
 
-    // Check if there's an actual argument and if it's a valid integer
     if (!arg || (*arg != '-' && *arg != '+' && !isdigit(*arg))) {
         fprintf(stderr, "L%u: usage: push integer\n", line_number);
         exit(EXIT_FAILURE);
     }
 
-    // Convert string to integer using strtol for better error handling
-    char *endptr;
-    value = strtol(arg, &endptr, 10);
-
-    // Check for conversion errors
-    if (*endptr != '\0' && !isspace(*endptr)) {
-        fprintf(stderr, "L%u: usage: push integer\n", line_number);
-        exit(EXIT_FAILURE);
+    int len = strlen(arg);
+    for (int i = 0; i < len; i++) {
+        if (!isdigit(arg[i]) && !(i == 0 && (arg[i] == '-' || arg[i] == '+'))) {
+            fprintf(stderr, "L%u: usage: push integer\n", line_number);
+            exit(EXIT_FAILURE);
+        }
     }
 
-    // Allocate memory for a new node
+    value = atoi(arg);
+
     stack_t *new_node = malloc(sizeof(stack_t));
     if (!new_node) {
         fprintf(stderr, "Error: malloc failed\n");
         exit(EXIT_FAILURE);
     }
 
-    // Update new node with value and link to the current stack
     new_node->n = value;
     new_node->prev = NULL;
     new_node->next = *stack;
 
-    // Update the previous node's prev pointer if the stack is not empty
     if (*stack) {
         (*stack)->prev = new_node;
     }
 
-    // Update the stack to point to the new node
     *stack = new_node;
 }
