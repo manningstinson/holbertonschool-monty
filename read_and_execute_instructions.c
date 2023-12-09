897bce7fd7d1f24f479a0f403e46f67e814ea7a0
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-09 15:52:42 -0600

    Update read_and_execute_instructions.c

--- read_and_execute_instructions.c
+++ read_and_execute_instructions.c
@@ -32,6 +32,7 @@ int read_and_execute_instructions(FILE *file) {
         } else if (strcmp(opcode, "pnit") == 0) {
             /* Handle pnit as a valid instruction without affecting the stack */
             printf("%s\n", opcode);
+            continue;  // Skip the rest of the loop
         } else if (strcmp(opcode, "pall") == 0) {
             /* Handle pall instruction */
             pall(&stack);
