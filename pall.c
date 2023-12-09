9e83f44d2a681d6587ad56b9685ae97ff840510a
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-09 17:25:11 -0600

    Update pall.c

--- pall.c
+++ pall.c
@@ -5,7 +5,7 @@ void pall(stack_t **stack) {
     stack_t *current = *stack;
 
     while (current) {
-        printf("%d\n", current->data);
+        printf("%d\n", current->n);
         current = current->next;
     }
 }
