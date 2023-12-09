25dd1889cf803c3e344cf48ae3cabf3d08d7a1a6
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-09 10:49:51 -0600

    Update monty.c

--- monty.c
+++ monty.c
@@ -1,16 +1,15 @@
+// monty.c
+
 #include "monty.h"
 
 int main(int argc, char *argv[]) {
-    // Check if the correct number of command-line arguments is provided
     if (argc != 2) {
-        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+        fprintf(stderr, "USAGE: %s file\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    // Open the Monty instruction file for reading
     FILE *file = fopen(argv[1], "r");
     if (!file) {
-        // Print an error message if the file cannot be opened
         fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
         exit(EXIT_FAILURE);
     }
@@ -20,4 +19,3 @@ int main(int argc, char *argv[]) {
 
     return EXIT_SUCCESS;
 }
-
