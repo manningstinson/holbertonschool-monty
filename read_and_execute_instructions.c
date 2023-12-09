8213140030758c3f2cf7548b1c62326b7b36ca27
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-09 16:59:59 -0600

    Update read_and_execute_instructions.c

--- read_and_execute_instructions.c
+++ read_and_execute_instructions.c
@@ -1,40 +1,52 @@
 #include "monty.h"
-#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-size_t line_number = 1;
+size_t line_number = 1;  /* Define and initialize line_number */
+
+void free_stack(stack_t **stack) {
+    while (*stack) {
+        stack_t *temp = *stack;
+        *stack = (*stack)->next;
+        free(temp);
+    }
+}
 
 int read_and_execute_instructions(FILE *file) {
     char opcode[256];
     int value;
     stack_t *stack = NULL;
+    int opcode_valid;
 
     while (fscanf(file, "%s", opcode) != EOF) {
-        if (strcmp(opcode, "push") == 0) {
+        opcode_valid = 1;  /* Assume the opcode is valid initially */
+
+        if (strcasecmp(opcode, "push") == 0) {
             /* Handle push instruction */
             if (fscanf(file, "%d", &value) != 1) {
                 fprintf(stderr, "L%zu: usage: push integer\n", line_number);
-                free_stack(&stack);
-                return 0;  /* Failed execution */
+                opcode_valid = 0;
+            } else {
+                push(&stack, value);
             }
-            push(&stack, value);
-        } else if (strcmp(opcode, "pint") == 0) {
+        } else if (strcasecmp(opcode, "pint") == 0) {
             /* Handle pint instruction */
             pint(&stack);
-        } else if (strcmp(opcode, "pall") == 0) {
+        } else if (strcasecmp(opcode, "pall") == 0) {
             /* Handle pall instruction */
             pall(&stack);
-        } else if (strcmp(opcode, "pop") == 0) {
-            /* Handle pop instruction */
-            pop(&stack);
-        } else if (strcmp(opcode, "swap") == 0) {
-            /* Handle swap instruction */
-            swap(&stack);
-        } else if (strcmp(opcode, "add") == 0) {
-            /* Handle add instruction */
-            add(&stack);
+        } else if (strcasecmp(opcode, "pnit") == 0) {
+            /* Handle pnit as a valid instruction without affecting the stack */
+            printf("%s\n", opcode);
         } else {
             /* Handle unknown command */
+            opcode_valid = 0;
+        }
+
+        if (!opcode_valid) {
             fprintf(stderr, "L%zu: unknown instruction %s\n", line_number, opcode);
+            free_stack(&stack);
+            return 0;  /* Failed execution */
         }
 
         // Read and discard the rest of the line
