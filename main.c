38f893946eb6da6b4e17a7b7546e95be1c993b65
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-09 17:09:49 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,29 +1,40 @@
 #include "monty.h"
-#include <stdio.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
-    /* Check for correct number of arguments */
-    if (argc != 2) {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
+size_t line_number = 1;
+
+int read_and_execute_instructions(FILE *file) {
+    char opcode[256];
+    int value;
+    stack_t *stack = NULL;
+
+    while (fscanf(file, "%s", opcode) != EOF) {
+        if (strcmp(opcode, "push") == 0) {
+            /* Handle push instruction */
+            if (fscanf(file, "%d", &value) != 1) {
+                fprintf(stderr, "L%zu: usage: push integer\n", line_number);
+                return 0;  /* Failed execution */
+            }
+            push(&stack, value);
+        } else if (strcmp(opcode, "pint") == 0) {
+            /* Handle pint instruction */
+            pint(&stack);
+        } else if (strcmp(opcode, "pall") == 0) {
+            /* Handle pall instruction */
+            pall(&stack);
+        } else {
+            /* Handle unknown command */
+            fprintf(stderr, "L%zu: unknown instruction %s\n", line_number, opcode);
+        }
+
+        // Read and discard the rest of the line
+        while (fgetc(file) != '\n');
+
+        line_number++;
     }
 
-    /* Open and read the Monty bytecode file */
-    FILE *file = fopen(argv[1], "r");
-    if (!file) {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
+    /* Free allocated memory and perform any cleanup if needed */
+    free(stack);
 
-    /* Read and execute instructions from the file */
-    if (!read_and_execute_instructions(file)) {
-        fprintf(stderr, "Error: Execution failed\n");
-        fclose(file);
-        exit(EXIT_FAILURE);
-    }
-
-    /* Close the file and free allocated memory if needed */
-    fclose(file);
-
-    return EXIT_SUCCESS;
+    return 1;  /* Successful execution */
 }
