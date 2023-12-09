c6df0de03de456b2212922a45de4e593a922a2c7
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-09 17:41:57 -0600

    Update push.c

--- push.c
+++ push.c
@@ -1,6 +1,4 @@
 #include "monty.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 void push(stack_t **stack, int value) {
     stack_t *new_node = malloc(sizeof(stack_t));
@@ -9,7 +7,7 @@ void push(stack_t **stack, int value) {
         exit(EXIT_FAILURE);
     }
 
-    new_node->n = value;
+    new_node->data = value;
     new_node->next = *stack;
     *stack = new_node;
 }
