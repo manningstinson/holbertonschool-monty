f0affe960843ff4e091f4153156b3d639f6237cb
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-09 17:01:58 -0600

    Update read_and_execute_instructions.c

--- read_and_execute_instructions.c
+++ read_and_execute_instructions.c
@@ -1,52 +1,30 @@
 #include "monty.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-size_t line_number = 1;  /* Define and initialize line_number */
-
-void free_stack(stack_t **stack) {
-    while (*stack) {
-        stack_t *temp = *stack;
-        *stack = (*stack)->next;
-        free(temp);
-    }
-}
+size_t line_number = 1;
 
 int read_and_execute_instructions(FILE *file) {
     char opcode[256];
     int value;
     stack_t *stack = NULL;
-    int opcode_valid;
 
     while (fscanf(file, "%s", opcode) != EOF) {
-        opcode_valid = 1;  /* Assume the opcode is valid initially */
-
-        if (strcasecmp(opcode, "push") == 0) {
+        if (strcmp(opcode, "push") == 0) {
             /* Handle push instruction */
             if (fscanf(file, "%d", &value) != 1) {
                 fprintf(stderr, "L%zu: usage: push integer\n", line_number);
-                opcode_valid = 0;
-            } else {
-                push(&stack, value);
+                return 0;  /* Failed execution */
             }
-        } else if (strcasecmp(opcode, "pint") == 0) {
+            push(&stack, value);
+        } else if (strcmp(opcode, "pint") == 0) {
             /* Handle pint instruction */
             pint(&stack);
-        } else if (strcasecmp(opcode, "pall") == 0) {
+        } else if (strcmp(opcode, "pall") == 0) {
             /* Handle pall instruction */
             pall(&stack);
-        } else if (strcasecmp(opcode, "pnit") == 0) {
-            /* Handle pnit as a valid instruction without affecting the stack */
-            printf("%s\n", opcode);
         } else {
             /* Handle unknown command */
-            opcode_valid = 0;
-        }
-
-        if (!opcode_valid) {
             fprintf(stderr, "L%zu: unknown instruction %s\n", line_number, opcode);
-            free_stack(&stack);
-            return 0;  /* Failed execution */
         }
 
         // Read and discard the rest of the line
@@ -56,7 +34,7 @@ int read_and_execute_instructions(FILE *file) {
     }
 
     /* Free allocated memory and perform any cleanup if needed */
-    free_stack(&stack);
+    free(stack);
 
     return 1;  /* Successful execution */
 }
