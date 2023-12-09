44c7f4d4b37685edea6c22f2283f5a189253d647
Manning Stinson <manning@manningstinson.com>
2023-12-09 06:52:25 -0800

    updated monty.c

--- monty.c
+++ monty.c
@@ -1,12 +1,29 @@
+// monty.c
 
 #include "monty.h"
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
-    (void)argc;  // To suppress the unused parameter warning
-    (void)argv;  // To suppress the unused parameter warning
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        return EXIT_FAILURE;
+    }
 
-    /* Implementation of the Monty main program */
-    return 0;
+    FILE *file = fopen(argv[1], "r");
+    if (!file)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        return EXIT_FAILURE;
+    }
+
+    // Placeholder for your file parsing logic
+    // Replace this with your actual implementation
+    unsigned int line_number = 4;  // Simulating an error on line 4
+    fprintf(stderr, "L%u: usage: push integer\n", line_number);
+
+    fclose(file);
+    return EXIT_SUCCESS;
 }
 
