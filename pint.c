ecc656ff5c4098a440925aae4c07d763c6501edd
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-09 17:50:04 -0600

    Update pint.c

--- pint.c
+++ pint.c
@@ -3,7 +3,7 @@
 
 void pint(stack_t **stack) {
     if (*stack) {
-        printf("%d\n", (*stack)->n);
+        printf("%d\n", (*stack)->data);  // Change 'n' to 'data'
     } else {
         fprintf(stderr, "L%zu: can't pint, stack empty\n", line_number);
         exit(EXIT_FAILURE);
