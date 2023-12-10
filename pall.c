819560a38f5e61825bed867494ea217bc6fdb905
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-09 18:24:47 -0600

    Update pall.c

--- pall.c
+++ pall.c
@@ -1,10 +1,10 @@
 #include "monty.h"
 
-void pint(stack_t **stack) {
-    if (*stack) {
-        printf("%d\n", (*stack)->data);
-    } else {
-        fprintf(stderr, "L%zu: can't pint, stack empty\n", line_number);
-        exit(EXIT_FAILURE);
+void pall(stack_t **stack) {
+    stack_t *current = *stack;
+
+    while (current) {
+        printf("%d\n", current->data);
+        current = current->next;
     }
 }
