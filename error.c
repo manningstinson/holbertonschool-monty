e76abacbad1cf7ef10cbb00f9e7ce53d47e91372
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-11 16:56:34 -0600

    Update error.c

--- error.c
+++ error.c
@@ -1,33 +1,29 @@
-#include "monty.h"
-#include <stdarg.h>
-#include <stdio.h>
-
-#define INVALID_BYTECODE 1
-#define STACK_UNDERFLOW 2
-#define INVALID_FILE 3
+void exit_error(int errcode, char *filename, const char *format, ...) __attribute__((format(printf, 3, 4)));
 
 void exit_error(int errcode, char *filename, const char *format, ...) {
-  va_list args;
-  va_start(args, format);
+    (void)errcode; // Unused, preventing a warning
+    (void)filename; // Unused, preventing a warning
 
-  switch (errcode) {
-    case INVALID_BYTECODE:
-      printf("Error: Unknown instruction\n");
-      break;
-    case STACK_UNDERFLOW:
-      printf("Error: Stack underflow\n");
-      break;
-    case INVALID_FILE:
-      printf("Error: Could not open bytecode file '%s'\n", filename);
-      break;
-    /* Add cases for other error types */
-  }
+    va_list args;
+    va_start(args, format);
 
-  if (format) {
-    vprintf(format, args);
-  }
+    switch (errcode) {
+        case INVALID_BYTECODE:
+            printf("Error: Unknown instruction\n");
+            break;
+        case STACK_UNDERFLOW:
+            printf("Error: Stack underflow\n");
+            break;
+        case INVALID_FILE:
+            printf("Error: Could not open bytecode file\n");
+            break;
+        /* Add cases for other error types */
+    }
 
-  va_end(args);
-  exit(errcode);
+    if (format) {
+        vprintf(format, args);
+    }
+
+    va_end(args);
+    exit(errcode);
 }
-
