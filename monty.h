d109f8967a4f9a06bd9566ce0933af21f48d754b
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-11 15:00:45 -0600

    Update monty.h

--- monty.h
+++ monty.h
@@ -1,24 +1,29 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdlib.h>
 
+/* Stack node structure */
 typedef struct stack_s {
   int n;
   struct stack_s *prev;
   struct stack_s *next;
 } stack_t;
 
+/* Instruction structure */
 typedef struct instruction_s {
   char *opcode;
-  void (*f)(stack_t **stack, int data); // Changed parameter type for push
+  void (*f)(stack_t **stack, int data);
 } instruction_t;
 
+/* Opcodes */
 #define PUSH 1
 #define POP 2
 #define PINT 3
 
+/* Function declarations */
 void push(stack_t **stack, int data);
 void pall(stack_t **stack);
 void pint(stack_t **stack);
@@ -28,8 +33,13 @@ void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack);
 void free_stack(stack_t **stack);
 
+/* Error handling function */
 void exit_error(int status, char *file, char *format, ...);
 
-bool stack_overflow(stack_t *stack); // Add definition for stack_overflow
+/* Function to check for stack overflow (implementation required) */
+bool stack_overflow(stack_t *stack);
+
+/* Function to push a node onto the stack (implementation required) */
+void stack_push(stack_t **stack, stack_t *node);
 
 #endif
