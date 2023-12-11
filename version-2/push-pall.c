3e143f5f6b112b98a3696be2b9027c802026345b
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-11 11:39:34 -0600

    Update push-pall.c

--- version-2/push-pall.c
+++ version-2/push-pall.c
@@ -5,22 +5,40 @@ void push(stack_t **stack, unsigned int line_number)
 	int value;
 	char *str;
 
-	if (!stack)
+	if (!stack) {
 		exit_error(EXIT_FAILURE, NULL, "L%u: can't push to NULL stack\n",
 			line_number);
+	}
 
 	str = strtok(NULL, " \t\n");
-	if (!str || sscanf(str, "%d", &value) != 1)
-		exit_error(EXIT_FAILURE, NULL, "L%u: usage: push integer\n",
-			line_number);
+	if (!str || sscanf(str, "%d", &value) != 1) {
+		exit_error(EXIT_FAILURE, NULL,
+			"L%u: usage: push integer\n", line_number);
+	}
+
+	if (stack_overflow(*stack)) {
+		exit_error(EXIT_FAILURE, NULL,
+			"L%u: stack overflow\n", line_number);
+	}
 
 	stack_push(*stack, value);
 }
 
-void pall(stack_t **stack, unsigned int line_number)
+void pall(stack_t **stack, unsigned int line_number, int order)
 {
-	if (!stack || !*stack)
-		return;
+	stack_t *node;
 
-	stack_print(*stack);
+	if (!stack || !*stack) {
+		return;
+	}
+
+	node = *stack;
+	if (order == PALL_TOP_DOWN) {
+		while (node) {
+			printf("%d\n", node->n);
+			node = node->next;
+		}
+	} else if (order == PALL_BOTTOM_UP) {
+		stack_print(*stack);
+	}
 }
