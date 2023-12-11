737c446c98b1b477811ff4e17d8998aeb2ae3722
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-11 11:42:37 -0600

    Update pint.c

--- version-2/pint.c
+++ version-2/pint.c
@@ -1,11 +1,15 @@
 #include "monty.h"
 
-void pint(stack_t **stack, unsigned int line_number)
+int pint(stack_t **stack, unsigned int line_number)
 {
     if (!*stack) {
-        fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
-        exit(EXIT_FAILURE);
+        exit_error(EXIT_FAILURE, NULL, "L%u: can't pint, stack empty\n",
+            line_number);
+        return EXIT_FAILURE;
     }
+
     printf("%d\n", (*stack)->n);
     pop(stack);
+
+    return EXIT_SUCCESS;
 }
