5ada8ffc689c9ea878d979c0bf86539de324e6ff
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-11 15:01:11 -0600

    Update push-pall.c

--- push-pall.c
+++ push-pall.c
@@ -1,34 +1,32 @@
 #include "monty.h"
-#include <stdio.h> // Added header for printf and strtok
+#include <stdio.h>
 
-void push(stack_t **stack, int data) {
-	int value;
+void push(stack_t **stack, int value) {
 	char *str;
 
 	if (!stack) {
-		exit_error(EXIT_FAILURE, NULL, "L%u: can't push to NULL stack\n",
-			line_number);
+		exit_error(EXIT_FAILURE, NULL, "can't push to NULL stack\n");
 	}
 
 	str = strtok(NULL, " \t\n");
 	if (!str || sscanf(str, "%d", &value) != 1) {
 		exit_error(EXIT_FAILURE, NULL,
-			"L%u: usage: push integer\n", line_number);
+			"usage: push integer\n");
 	}
 
 	if (stack_overflow(*stack)) {
 		exit_error(EXIT_FAILURE, NULL,
-			"L%u: stack overflow\n", line_number);
+			"stack overflow\n");
 	}
 
 	stack_t *new_node = malloc(sizeof(stack_t));
 	if (!new_node) {
 		exit_error(EXIT_FAILURE, NULL,
-			"L%u: Error allocating memory\n", line_number);
+			"Error allocating memory\n");
 	}
 
 	new_node->n = value;
-	stack_push(*stack, new_node); // Use stack_push if needed
+	stack_push(*stack, new_node);
 }
 
 void pall(stack_t **stack) {
