d75e44060635b34a542f3f5d45b8ac22da9b55b9
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-11 13:37:44 -0600

    Update add.c

--- version-2/add.c
+++ version-2/add.c
@@ -1,15 +1,15 @@
-#include "monty.h"
+#include "monty.h" /* Includes necessary header file for data structures and function definitions */
 
-int add(stack_t **stack, unsigned int line_number)
+int add(stack_t **stack, unsigned int line_number) /* Function definition for `add` operation */
 {
-    if (!*stack || !(*stack)->next) {
-        fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
-        return EXIT_FAILURE;
+    if (!*stack || !(*stack)->next) { /* Check if stack is empty or only has one element */
+        fprintf(stderr, "L%u: can't add, stack too short\n", line_number); /* Print error message if stack is too short */
+        return EXIT_FAILURE; /* Return error code */
     }
 
-    stack_t *node = (*stack)->next;
-    node->n += (*stack)->n;
-    pop(stack);
+    stack_t *node = (*stack)->next; /* Get a pointer to the second element */
+    node->n += (*stack)->n; /* Add the top element's value to the second element's value */
+    pop(stack); /* Remove the top element from the stack */
 
-    return EXIT_SUCCESS;
+    return EXIT_SUCCESS; /* Return success code */
 }
