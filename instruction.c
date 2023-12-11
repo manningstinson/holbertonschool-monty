65ff586438a8d94cf6a9302b509de6cd1e27d951
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-10 19:41:09 -0600

    Update instruction.c

--- instruction.c
+++ instruction.c
@@ -11,3 +11,26 @@ Instruction parse_instruction(int bytecode) {
     default: return UNKNOWN;
   }
 }
+
+void execute_instruction(Instruction instruction, Stack **top, int value) {
+  switch (instruction) {
+    case PUSH: push(top, value); break;
+    case POP: {
+      int result = pop(top);
+      if (result != -1) {
+        printf("%d\n", result);
+      }
+    } break;
+    case PINT: {
+      int result = pop(top);
+      if (result != -1) {
+        printf("%d\n", result);
+        // You might want to push the value back onto the stack if needed.
+      }
+    } break;
+    // Add cases for other instructions as needed
+    default:
+      // Handle unknown instructions or other cases
+      break;
+  }
+}
