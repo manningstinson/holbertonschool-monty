2488bb477c92dc6d016971ec1f6188548e427d8f
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-11 13:24:32 -0600

    Update instruction.c

--- version-2/instruction.c
+++ version-2/instruction.c
@@ -1,35 +1,47 @@
 #include "monty.h"
 
+#include <stdlib.h>
+#include <string.h>
+
+/* Define a struct for mapping opcodes to functions and error messages */
+typedef struct opcode_mapping {
+  char *opcode;
+  void (*f)(stack_t **stack, unsigned int line_number);
+  char *error_msg;
+} opcode_mapping_t;
+
+/* Create an array of opcode mappings */
+opcode_mapping_t opcode_map[] = {
+  {"push", &push, "Invalid input format for push"},
+  {"pop", &pop, "Stack underflow"},
+  {"pint", &pint, "Stack underflow"},
+  {"pall", &pall, NULL},
+  {"nop", &nop, NULL},
+  {"add", &add, "Stack underflow"},
+  {"swap", &swap, "Stack too short"},
+  // Add more mappings for new opcodes
+  // ...
+  {NULL, NULL, "Unknown instruction"}
+};
+
 instruction_t parse_instruction(char *opcode) {
   instruction_t instruction;
 
-  /* Map string opcodes to instruction structures */
-  if (strcmp(opcode, "push") == 0) {
-    instruction.opcode = "push";
-    instruction.f = &push;
-  } else if (strcmp(opcode, "pop") == 0) {
-    instruction.opcode = "pop";
-    instruction.f = &pop;
-  } else if (strcmp(opcode, "pint") == 0) {
-    instruction.opcode = "pint";
-    instruction.f = &pint;
-  } else if (strcmp(opcode, "pall") == 0) {
-    instruction.opcode = "pall";
-    instruction.f = &pall;
-  } else if (strcmp(opcode, "nop") == 0) {
-    instruction.opcode = "nop";
-    instruction.f = &nop;
-  } else if (strcmp(opcode, "add") == 0) {
-    instruction.opcode = "add";
-    instruction.f = &add;
-  } else if (strcmp(opcode, "swap") == 0) {
-    instruction.opcode = "swap";
-    instruction.f = &swap;
-  } else {
-    // Handle unknown instruction
-    instruction.opcode = "unknown";
-    instruction.f = NULL;
+  /* Use a hash table for faster lookup (optional) */
+  // ...
+
+  /* Linear search through the opcode map */
+  for (int i = 0; opcode_map[i].opcode; i++) {
+    if (strcmp(opcode, opcode_map[i].opcode) == 0) {
+      instruction.opcode = opcode_map[i].opcode;
+      instruction.f = opcode_map[i].f;
+      return instruction;
+    }
   }
 
-  return instruction;
+  /* Handle unknown opcode */
+  instruction.opcode = opcode_map[i].opcode;
+  instruction.f = NULL;
+  fprintf(stderr, "L%u: %s\n", line_number, opcode_map[i].error_msg);
+  exit(EXIT_FAILURE);
 }
