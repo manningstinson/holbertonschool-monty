66c8b258de8049f82eae1431c148a54c01d90e4f
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-11 14:21:49 -0600

    Update add.c

--- add.c
+++ add.c
@@ -1,6 +1,7 @@
 #include "monty.h"
 
-void add(stack_t **stack, unsigned int line_number) {
+void add(stack_t **stack, unsigned int line_number)
+{
     if (!*stack || !(*stack)->next) {
         fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
         exit(EXIT_FAILURE);
