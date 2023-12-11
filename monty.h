96553c75e65cd031f838d147927303eefcc75b4c
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-11 16:28:17 -0600

    Update monty.h

--- monty.h
+++ monty.h
@@ -48,13 +48,13 @@ instruction_t parse_instruction(int bytecode);
 int execute_simple_instruction(instruction_t instruction, stack_t **top, unsigned int line_number);
 
 /* Define instruction enum without tag */
-typedef enum {
+enum instruction_type {
   PUSH,
   POP,
   PINT,
   /* ... other instructions ... */
   UNKNOWN = -1
-} instruction_type;
+};
 
 
 #endif
