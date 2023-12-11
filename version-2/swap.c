631f2eea8161f1c9736cfc74d7293ea2a35dbb37
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-11 13:14:41 -0600

    Update swap.c

--- version-2/swap.c
+++ version-2/swap.c
@@ -2,21 +2,18 @@
 
 int swap(stack_t **stack, unsigned int line_number)
 {
-    stack_t *tmp;
-
     if (!*stack || !(*stack)->next) {
         exit_error(EXIT_FAILURE, NULL, "L%u: can't swap, stack too short\n",
             line_number);
         return EXIT_FAILURE;
     }
 
-    tmp = (*stack)->next;
-    (*stack)->next = tmp->next;
-    tmp->next->prev = *stack;
-    tmp->prev = NULL;
-    tmp->next = *stack;
-    (*stack)->prev = tmp;
-    *stack = tmp;
+    /* Avoid unnecessary temporary variable */
+    (*stack)->next->prev = *stack;
+    (*stack)->prev = (*stack)->next;
+    (*stack)->next = (*stack)->next->next;
+    (*stack)->prev->next = *stack;
+    *stack = (*stack)->prev;
 
     return EXIT_SUCCESS;
 }
