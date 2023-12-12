63ad993fa2701e47a697d10e15c259cf4dea03c2
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-11 18:15:53 -0600

    Update monty.h

--- monty.h
+++ monty.h
@@ -29,7 +29,7 @@ typedef struct instruction_s {
 
 /* Function declarations */
 void push(stack_t **stack, int data);
-void pall(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
