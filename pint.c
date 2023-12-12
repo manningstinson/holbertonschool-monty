ec88729f7f992e28ef000d442bd5f381f443ae43
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-11 19:22:03 -0600

    Update pint.c

--- pint.c
+++ pint.c
@@ -1,14 +1,8 @@
-#include <stdio.h>
 #include "monty.h"
 
-/**
- * pint - Prints the value at the top of the stack.
- * @stack: Double pointer to the beginning of the stack.
- * @line_number: Line number of the current operation.
- */
-void pint(stack_t **stack, unsigned int line_number)
+void pint(stack_t **stack, int line_number)
 {
-    if (!stack || !(*stack))
+    if (!*stack)
     {
         fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
         exit_error(EXIT_FAILURE, NULL, NULL);
