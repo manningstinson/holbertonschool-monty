a9f8c7c5b3937f28964da2b0f90bfb080baaf236
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-11 19:06:46 -0600

    Create nop.c

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - Does nothing.
+ * @stack: Double pointer to the beginning of the stack.
+ * @line_number: Line number of the current operation.
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+    (void)stack;
+    (void)line_number;
+    /* Do nothing */
+}
