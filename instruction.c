70fc22d90812c544630b097ba1fc57bf199e6ccd
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-11 18:49:15 -0600

    Update instruction.c

--- instruction.c
+++ instruction.c
@@ -1,13 +1,11 @@
 #include "monty.h"
 
 instruction_t instructions[] = {
-    {"push", &push},
-    {"pall", &pall},
-    {"pint", &pint},
-    {"pop", &pop},
-    {"swap", &swap},
-    {"add", &add},
-    {"nop", &nop},
+    {"pall", pall},
+    {"pint", pint},
+    {"pop", pop},
+    {"swap", swap},
+    {"add", add},
+    {"nop", nop},
     {NULL, NULL}
 };
-
