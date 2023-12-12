4cf4bb4016b7b705e32ae4bae56faf6f71b6b93c
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-11 18:46:00 -0600

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,10 +1,9 @@
 #include "monty.h"
 
-void swap(stack_t **stack, unsigned int line_number){
+void swap(stack_t **stack, unsigned int line_number) {
     if (!*stack || !(*stack)->next) {
-        exit_error(EXIT_FAILURE, NULL, "L%u: can't swap, stack too short\n",
-            line_number);
-        return EXIT_FAILURE;
+        exit_error(EXIT_FAILURE, NULL, "L%u: can't swap, stack too short\n", line_number);
+        return;  // No need for return statements in a void function
     }
 
     /* Avoid unnecessary temporary variable */
@@ -13,6 +12,4 @@ void swap(stack_t **stack, unsigned int line_number){
     (*stack)->next = (*stack)->next->next;
     (*stack)->prev->next = *stack;
     *stack = (*stack)->prev;
-
-    return EXIT_SUCCESS;
 }
