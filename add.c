ea9133203a83ee91f6a4f057aa83ff604e6b405e
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-13 17:43:12 -0600

    Update add.c

--- add.c
+++ add.c
@@ -5,7 +5,11 @@ void add(stack_t **stack, unsigned int line_number) {
         exit_error(EXIT_FAILURE, NULL, "L%u: can't add, stack too short\n", line_number);
     }
 
+    printf("Before addition: %d + %d\n", (*stack)->n, (*stack)->next->n); // Debugging statement
+
     stack_t *node = (*stack)->next;
     node->n += (*stack)->n;
     pop(stack, line_number);  // Pass line_number to pop function
+
+    printf("After addition: %d\n", node->n); // Debugging statement
 }
