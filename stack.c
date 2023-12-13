e97b7f75c91266a3a0195b9b775bc09bff6adedc
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-13 11:25:23 -0600

    Update stack.c

--- stack.c
+++ stack.c
@@ -1,28 +1,6 @@
 #include <stdio.h>
 #include "monty.h"
 
-void pall(stack_t **stack, int line_number __attribute__((unused)))
-{
-    stack_t *node = *stack;
-
-    while (node)
-    {
-        printf("%d\n", node->n);
-        node = node->next;
-    }
-}
-
-void pint(stack_t **stack, int line_number)
-{
-    if (!*stack)
-    {
-        fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
-        exit_error(EXIT_FAILURE, NULL, NULL);
-    }
-
-    printf("%d\n", (*stack)->n);
-}
-
 void pop(stack_t **stack, int line_number)
 {
     if (!*stack)
