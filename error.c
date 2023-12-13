a06345d0681abeec9eaaa66f5f0240744d83926e
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-13 13:38:20 -0600

    Update error.c

--- error.c
+++ error.c
@@ -1,3 +1,5 @@
+// error.c
+
 #include "monty.h"
 #include <stdarg.h>
 #include <stdio.h>
@@ -9,22 +11,22 @@ void exit_error(int errcode, const char *format, ...) {
 
     switch (errcode) {
         case INVALID_BYTECODE:
-            printf("Error: Unknown instruction\n");
+            fprintf(stderr, "Error: Unknown instruction\n");
             break;
         case STACK_UNDERFLOW:
-            printf("Error: Stack underflow\n");
+            fprintf(stderr, "Error: Stack underflow\n");
             break;
         case INVALID_FILE:
-            printf("Error: Could not open bytecode file\n");
+            fprintf(stderr, "Error: Could not open bytecode file\n");
             break;
         /* Add cases for other error types */
         default:
-            printf("Error: Unknown error code\n");
+            fprintf(stderr, "Error: Unknown error code\n");
             break;
     }
 
     if (format) {
-        vprintf(format, args);
+        vfprintf(stderr, format, args);
     }
 
     va_end(args);
