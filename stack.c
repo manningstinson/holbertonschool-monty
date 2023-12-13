f07610a67f00bdc8c0b80542e3f14e204823ff72
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-13 11:55:08 -0600

    Update stack.c

--- stack.c
+++ stack.c
@@ -1,1 +1,35 @@
+#include "monty.h"
+
+stack_t *create_stack(void) {
+    stack_t *new_node = malloc(sizeof(stack_t));
+    if (!new_node) {
+        exit_error(EXIT_FAILURE, NULL, "Memory allocation error\n");
+    }
+    return new_node;
+}
+
+
+void free_stack(stack_t **stack) {
+    while (*stack) {
+        stack_t *temp = *stack;
+        *stack = (*stack)->next;
+        free(temp);
+    }
+}
+
+bool stack_overflow(stack_t *stack) {
+    // Your stack overflow logic goes here
+    // Return true if overflowed, false otherwise
+    return false;
+}
+
+
+void stack_push(stack_t **stack, stack_t *node) {
+    if (*stack != NULL) {
+        (*stack)->prev = node;
+    }
+    *stack = node;
+}
+
+
 
