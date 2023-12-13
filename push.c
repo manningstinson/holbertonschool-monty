013b83facb35589cf4af94fac06db9dd8086fa1b
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-13 13:50:56 -0600

    Update push.c

--- push.c
+++ push.c
@@ -4,14 +4,21 @@
 #include <string.h>
 
 void push(stack_t **stack, unsigned int line_number) {
-   char *str = NULL; /* Initialize str */
+    (void)line_number; // Unused parameter, added to suppress the warning
+
+    char *str = NULL; /* Initialize str */
 
     if (!stack) {
         exit_error(EXIT_FAILURE, NULL, "can't push to NULL stack\n");
     }
 
     str = strtok(NULL, " \t\n");
-    if (!str || sscanf(str, "%d", &value) != 1) {
+    if (!str) {
+        exit_error(EXIT_FAILURE, NULL, "usage: push integer\n");
+    }
+
+    int value;
+    if (sscanf(str, "%d", &value) != 1) {
         exit_error(EXIT_FAILURE, NULL, "usage: push integer\n");
     }
 
