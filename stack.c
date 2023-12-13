af213740e3ac861c860a3eee7309aea2a8999599
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-13 11:50:25 -0600

    Update stack.c

--- stack.c
+++ stack.c
@@ -1,17 +1,1 @@
-#include <stdio.h>
-#include "monty.h"
 
-void swap(stack_t **stack, int line_number)
-{
-    if (!*stack || !(*stack)->next)
-    {
-        exit_error(EXIT_FAILURE, NULL, "L%u: can't swap, stack too short\n", line_number);
-    }
-
-    stack_t *tmp = (*stack)->next;
-    (*stack)->next = tmp->next;
-    tmp->next = *stack;
-    (*stack)->prev = tmp;
-    tmp->prev = NULL;
-    *stack = tmp;
-}
