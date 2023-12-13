95094eb8de56929cab38c21cc285a3f611748df5
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-13 11:56:02 -0600

    Update instruction.c

--- instruction.c
+++ instruction.c
@@ -1,11 +1,34 @@
 #include "monty.h"
 
-instruction_t instructions[] = {
-    {"pall", pall},
-    {"pint", pint},
-    {"pop", pop},
-    {"swap", swap},
-    {"add", add},
-    {"nop", nop},
-    {NULL, NULL}
-};
+instruction_t parse_instruction(int bytecode) {
+    instruction_t instruction;
+
+    switch (bytecode) {
+        case PUSH_OPCODE:
+            instruction.opcode = "push";
+            instruction.f = push;
+            break;
+        case POP_OPCODE:
+            instruction.opcode = "pop";
+            instruction.f = pop;
+            break;
+        case PINT_OPCODE:
+            instruction.opcode = "pint";
+            instruction.f = pint;
+            break;
+        // Add other cases for different opcodes
+        default:
+            instruction.opcode = "unknown";
+            instruction.f = nop;  // Default to no operation
+            break;
+    }
+
+    return instruction;
+}
+
+#include "monty.h"
+
+int execute_simple_instruction(instruction_t instruction, stack_t **top, unsigned int line_number) {
+    instruction.f(top, line_number);
+    return 0;  
+}
