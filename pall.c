d6764740e3999947d9dabb4752e5b3bb85e6cb02
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-13 11:21:11 -0600

    Update pall.c

--- pall.c
+++ pall.c
@@ -1,1 +1,12 @@
+#include "monty.h"
 
+void pall(stack_t **stack, int line_number __attribute__((unused)))
+{
+    stack_t *node = *stack;
+
+    while (node)
+    {
+        printf("%d\n", node->n);
+        node = node->next;
+    }
+}
