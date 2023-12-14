8644c02113dfea714fa59d7d358edf78b15f1931
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-13 19:04:49 -0600

    Update nop.c

--- nop.c
+++ nop.c
@@ -3,5 +3,7 @@
 void nop(stack_t **stack, unsigned int line_number) {
     (void)stack;
     (void)line_number;
+    
     // No operation (NOP) - do nothing
+    printf("Executing NOP at line %u\n", line_number);
 }
