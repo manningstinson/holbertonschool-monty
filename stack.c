7169cfd3557c20b64b28621aeaf02a1978e5b8be
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-13 19:08:53 -0600

    Update stack.c

--- stack.c
+++ stack.c
@@ -3,33 +3,37 @@
 stack_t *create_stack(void) {
     stack_t *new_node = malloc(sizeof(stack_t));
     if (!new_node) {
-        exit_error(EXIT_FAILURE, NULL, "Memory allocation error\n");
+        exit_error(EXIT_FAILURE, NULL, "Memory allocation error in create_stack\n");
     }
+
+    new_node->n = 0;        // Initialize the value to 0 or any default value
+    new_node->prev = NULL;
+    new_node->next = NULL;
+
+    printf("Created a new stack at address: %p\n", (void *)new_node);
+
     return new_node;
 }
-
 
 void free_stack(stack_t **stack) {
     while (*stack) {
         stack_t *temp = *stack;
         *stack = (*stack)->next;
         free(temp);
+        printf("Freed a node from the stack\n");
     }
 }
 
-
 bool stack_overflow(stack_t *stack __attribute__((unused))) {
-     return false; 
+    return false;  // Placeholder for overflow condition; adjust as needed
 }
-
-
 
 void stack_push(stack_t **stack, stack_t *node) {
     if (*stack != NULL) {
         (*stack)->prev = node;
+        node->next = *stack;
     }
     *stack = node;
+
+    printf("Pushed a node onto the stack with value: %d\n", node->n);
 }
-
-
-
