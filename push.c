1ef5caaecb9b477b8d43dcb3ae3cdd459ae5d018
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-13 19:07:11 -0600

    Update push.c

--- push.c
+++ push.c
@@ -9,26 +9,28 @@ void push(stack_t **stack, unsigned int line_number) {
     char *str = NULL; /* Initialize str */
 
     if (!stack) {
-        exit_error(EXIT_FAILURE, NULL, "can't push to NULL stack\n");
+        exit_error(EXIT_FAILURE, NULL, "L%u: can't push to NULL stack\n", line_number);
     }
 
     str = strtok(NULL, " \t\n");
     if (!str) {
-        exit_error(EXIT_FAILURE, NULL, "usage: push integer\n");
+        exit_error(EXIT_FAILURE, NULL, "L%u: usage: push integer\n", line_number);
     }
 
     int value;
     if (sscanf(str, "%d", &value) != 1) {
-        exit_error(EXIT_FAILURE, NULL, "usage: push integer\n");
+        exit_error(EXIT_FAILURE, NULL, "L%u: usage: push integer\n", line_number);
     }
 
     if (stack_overflow(*stack)) {
-        exit_error(EXIT_FAILURE, NULL, "stack overflow\n");
+        exit_error(EXIT_FAILURE, NULL, "L%u: stack overflow\n", line_number);
     }
+
+    printf("Pushing element at line %u: %d\n", line_number, value);
 
     stack_t *new_node = malloc(sizeof(stack_t));
     if (!new_node) {
-        exit_error(EXIT_FAILURE, NULL, "Error allocating memory\n");
+        exit_error(EXIT_FAILURE, NULL, "L%u: Error allocating memory\n", line_number);
     }
 
     new_node->n = value;
