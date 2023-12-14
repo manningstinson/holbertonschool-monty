b0f436290ef9df9427cc7e7c55e2345cd76cace9
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-13 19:28:55 -0600

    Update push.c

--- push.c
+++ push.c
@@ -6,24 +6,22 @@
 void push(stack_t **stack, unsigned int line_number) {
     (void)line_number; // Unused parameter, added to suppress the warning
 
-    char *str = NULL; /* Initialize str */
-
-    if (!stack || !*stack) {
-        exit_error(EXIT_FAILURE, NULL, "L%u: can't push to NULL or empty stack\n", line_number);
-    }
-
-    str = strtok(NULL, " \t\n");
+    char *str = strtok(NULL, " \t\n");
     if (!str) {
         exit_error(EXIT_FAILURE, NULL, "L%u: usage: push integer\n", line_number);
     }
 
-    int value;
-    if (sscanf(str, "%d", &value) != 1) {
+    // Ensure the entire string is a valid integer
+    char *endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (*endptr != '\0' && *endptr != '\n') {
         exit_error(EXIT_FAILURE, NULL, "L%u: usage: push integer\n", line_number);
     }
 
     printf("Pushing element at line %u: %d\n", line_number, value);
 
+    // Rest of your push logic...
     stack_t *new_node = malloc(sizeof(stack_t));
     if (!new_node) {
         exit_error(EXIT_FAILURE, NULL, "L%u: Error allocating memory for stack node\n", line_number);
