54546304de11e99467b8874232e1c0241945b8fd
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-13 18:39:00 -0600

    Update pall.c

--- pall.c
+++ pall.c
@@ -1,11 +1,17 @@
 #include "monty.h"
 #include <stdio.h>
 
-// pall.c
+void pall(stack_t **stack, unsigned int line_number) {
+    (void)line_number; // Unused parameter, added to suppress the warning
 
-#include "monty.h"
+    if (*stack == NULL) {
+        return; // If the stack is empty, do nothing
+    }
 
-void pall(stack_t **stack, unsigned int line_number __attribute__((unused))) {
-  
-    (void)stack; // Unused parameter, added to suppress the warning
+    stack_t *current = *stack;
+
+    while (current != NULL) {
+        printf("%d\n", current->n);
+        current = current->next;
+    }
 }
