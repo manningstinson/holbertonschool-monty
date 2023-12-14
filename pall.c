2c5b7d35483a2d113783032be335e0b7a3499f10
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-13 19:05:37 -0600

    Update pall.c

--- pall.c
+++ pall.c
@@ -5,8 +5,11 @@ void pall(stack_t **stack, unsigned int line_number) {
     (void)line_number; // Unused parameter, added to suppress the warning
 
     if (*stack == NULL) {
+        printf("Stack is empty\n");
         return; // If the stack is empty, do nothing
     }
+
+    printf("Printing stack at line %u:\n", line_number);
 
     stack_t *current = *stack;
 
