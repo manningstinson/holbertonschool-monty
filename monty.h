79d9a46a21d253bd5194d11979743cbed3df11d1
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-16 11:06:46 -0600

    Update monty.h

--- monty.h
+++ monty.h
@@ -45,6 +45,7 @@ void op_pint(stack_t **stack, unsigned int line_number);
 void op_pop(stack_t **stack, unsigned int line_number);
 void op_swap(stack_t **stack, unsigned int line_number);
 void op_add(stack_t **stack, unsigned int line_number);
+void op_nop(stack_t **stack, unsigned int line_number);
 
 
 
