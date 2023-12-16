83714f1a805d1ce212edb400f6305d366f53f856
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-16 09:07:54 -0600

    Update file_handling.c

--- file_handling.c
+++ file_handling.c
@@ -2,31 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int read_file(char *filename, stack_t **stack) {
-    FILE *file = fopen(filename, "r");
-
-    if (!file) {
-        fprintf(stderr, "Error: Can't open file %s\n", filename);
-        return EXIT_FAILURE;
-    }
-
-    char *line = NULL;
-    size_t len = 0;
-    unsigned int line_number = 0;
-
-    while (getline(&line, &len, file) != -1) {
-        line_number++;
-        opcode_dispatcher(line, stack, line_number);
-    }
-
-    free(line);
-    fclose(file);
-    return EXIT_SUCCESS;
-}
-#include "monty.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
  * read_file - Reads the content of a file and executes Monty byte codes.
  * @filename: Name of the file to read
