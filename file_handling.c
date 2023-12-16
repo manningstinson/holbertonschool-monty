5c127da6a417128463c9c21ff3990db3d2f5a562
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-16 07:45:57 -0600

    Update file_handling.c

--- file_handling.c
+++ file_handling.c
@@ -1,6 +1,15 @@
 #include "monty.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>  // Include for string functions
 
+/**
+ * read_file - Reads the content of a file and executes Monty byte codes
+ * @filename: Name of the file to read
+ * @stack: Double pointer to the stack
+ *
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
+ */
 int read_file(char *filename, stack_t **stack) {
     FILE *file = fopen(filename, "r");
 
@@ -9,8 +18,17 @@ int read_file(char *filename, stack_t **stack) {
         return EXIT_FAILURE;
     }
 
-    /* Rest of your code for reading the file */
+    char *line = NULL;
+    size_t len = 0;
+    unsigned int line_number = 0;
 
+    while (getline(&line, &len, file) != -1) {
+        line_number++;
+        /* Process the line and execute Monty byte codes */
+        opcode_dispatcher(line, stack, line_number);
+    }
+
+    free(line);
     fclose(file);
     return EXIT_SUCCESS;
 }
