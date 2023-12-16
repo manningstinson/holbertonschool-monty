fbb87424762bed4623017cda911aab608387a69f
Manning Stinson <manning@manningstinson.com>
2023-12-16 09:18:18 -0800

    updated betty styles

--- file_handling.c
+++ file_handling.c
@@ -11,31 +11,30 @@
  */
 int read_file(char *filename, stack_t **stack)
 {
-    FILE *file = fopen(filename, "r");
+	FILE *file = fopen(filename, "r");
 
-    if (!file)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", filename);
-        return EXIT_FAILURE;
-    }
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", filename);
+		return (EXIT_FAILURE);
+	}
 
-    char *line = NULL;
-    size_t len = 0;
-    unsigned int line_number = 0;
+	char *line = NULL;
+	size_t len = 0;
+	unsigned int line_number = 0;
 
-    while (getline(&line, &len, file) != -1)
-    {
-        line_number++;
-        /* Remove newline character at the end of the line */
-        size_t line_len = strlen(line);
-        if (line_len > 0 && line[line_len - 1] == '\n')
-            line[line_len - 1] = '\0';
+	while (getline(&line, &len, file) != -1)
+	{
+		line_number++;
+		/* Remove newline character at the end of the line */
+		size_t line_len = strlen(line);
 
-        /* Process the line and execute Monty byte codes */
-        opcode_dispatcher(line, stack, line_number);
-    }
-
-    free(line);
-    fclose(file);
-    return EXIT_SUCCESS;
+		if (line_len > 0 && line[line_len - 1] == '\n')
+			line[line_len - 1] = '\0';
+		/* Process the line and execute Monty byte codes */
+		opcode_dispatcher(line, stack, line_number);
+	}
+	free(line);
+	fclose(file);
+	return (EXIT_SUCCESS);
 }
