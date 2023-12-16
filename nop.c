756afde129bd6c600d39386ae4a66c4e9f0356b3
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-16 11:03:39 -0600

    Create nop.c

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+#include <stdio.h>
+
+/**
+ * op_nop - Doesn't do anything.
+ * @stack: Double pointer to the stack
+ * @line_number: Line number in the Monty byte code file
+ */
+void op_nop(stack_t **stack, unsigned int line_number) {
+    (void)stack;
+    (void)line_number;
+    /* No operation, just skip */
+}
