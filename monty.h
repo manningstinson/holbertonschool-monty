a8ff281bc17c49655451857b523760870b148b3f
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-16 10:51:57 -0600

    Update monty.h

--- monty.h
+++ monty.h
@@ -43,6 +43,8 @@ void free_stack(stack_t *stack);
 void cleanup(stack_t *stack);
 void op_pint(stack_t **stack, unsigned int line_number);
 void op_pop(stack_t **stack, unsigned int line_number);
+void op_swap(stack_t **stack, unsigned int line_number);
+
 
 
 #endif /* MONTY_H */
