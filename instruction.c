a700be5974b4c83e97ca3ee5f58a821966030531
Manning Stinson <manning@manningstinson.com>
2023-12-16 09:37:46 -0800

    updating betty styles

--- instruction.c
+++ instruction.c
@@ -15,29 +15,47 @@
  *
  * Return: None.
  */
-void opcode_dispatcher(char *line, stack_t **stack, unsigned int line_number) {
-    char *opcode = strtok(line, " \t\n");
 
-    if (!opcode) {
-        return; // Ignore empty lines
-    }
+void opcode_dispatcher(char *line, stack_t **stack, unsigned int line_number)
+{
+	char *opcode = strtok(line, " \t\n");
 
-    if (strcmp(opcode, "push") == 0) {
-        op_push(stack, line_number);
-    } else if (strcmp(opcode, "pall") == 0) {
-        op_pall(stack, line_number);
-    } else if (strcmp(opcode, "pint") == 0) {
-        op_pint(stack, line_number);
-    } else if (strcmp(opcode, "pop") == 0) {
-        op_pop(stack, line_number);
-    } else if (strcmp(opcode, "swap") == 0) {
-        op_swap(stack, line_number);
-    } else if (strcmp(opcode, "add") == 0) {
-        op_add(stack, line_number);
-    } else if (strcmp(opcode, "nop") == 0) {
-        op_nop(stack, line_number);
-    } else {
-        fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
-        exit(EXIT_FAILURE);
-    }
+	if (!opcode)
+	{
+		return;
+	}
+
+	if (strcmp(opcode, "push") == 0)
+	{
+		op_push(stack, line_number);
+	}
+	else if (strcmp(opcode, "pall") == 0)
+	{
+		op_pall(stack, line_number);
+	}
+	else if (strcmp(opcode, "pint") == 0)
+	{
+		op_pint(stack, line_number);
+	}
+	else if (strcmp(opcode, "pop") == 0)
+	{
+		op_pop(stack, line_number);
+	}
+	else if (strcmp(opcode, "swap") == 0)
+	{
+		op_swap(stack, line_number);
+	}
+	else if (strcmp(opcode, "add") == 0)
+	{
+		op_add(stack, line_number);
+	}
+	else if (strcmp(opcode, "nop") == 0)
+	{
+		op_nop(stack, line_number);
+	}
+	else
+	{
+		fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+		exit(EXIT_FAILURE);
+	}
 }
