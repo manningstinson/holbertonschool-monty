c310fd86ef3a61da7363ab7ea7fc90eb4b60da97
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-16 08:34:45 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,12 @@
 #include "monty.h"
-#include <stdlib.h>
-#include <stdio.h>
+#include <stdlib.h>  /* For EXIT_FAILURE and EXIT_SUCCESS */
+#include <stdio.h>   /* For fprintf */
 
-/**
- * cleanup_wrapper - Wrapper function for atexit with the correct signature
- */
+/* Wrapper function for atexit with the correct signature */
 void cleanup_wrapper(void) {
-    cleanup();  // Assuming cleanup takes no arguments
+    cleanup(NULL);  // Passing NULL or any appropriate argument
 }
 
-/**
- * main - Entry point for the Monty interpreter
- * @argc: Number of command-line arguments
- * @argv: Array of command-line arguments
- *
- * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
- */
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "USAGE: monty file\n");
@@ -27,10 +18,13 @@ int main(int argc, char *argv[]) {
 
     if (read_file(filename, &stack) == EXIT_FAILURE) {
         fprintf(stderr, "Error: Can't open file %s\n", filename);
+        cleanup(stack);  // Cleanup on failure
         return EXIT_FAILURE;
     }
 
     atexit(cleanup_wrapper);
+
+    cleanup(stack);  // Cleanup before exiting
 
     return EXIT_SUCCESS;
 }
