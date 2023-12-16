8bb2fb559c0e16c20903b37bb16d2d56a7c8f177
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-16 08:40:39 -0600

    Update monty.h

--- monty.h
+++ monty.h
@@ -14,9 +14,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+    int n;
+    struct stack_s *prev;
+    struct stack_s *next;
 } stack_t;
 
 /**
@@ -29,8 +29,8 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+    char *opcode;
+    void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 /* Function Prototypes */
@@ -43,4 +43,3 @@ void free_stack(stack_t *stack);
 void cleanup(stack_t *stack);
 
 #endif /* MONTY_H */
-
