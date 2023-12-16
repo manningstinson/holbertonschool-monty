33ac57e285046bc0686c438c756239b854e8771e
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-16 07:47:43 -0600

    Update push.c

--- push.c
+++ push.c
@@ -1,24 +1,41 @@
 #include "monty.h"
-#include <stdlib.h>
 #include <stdio.h>
-#include <ctype.h>  // Include for isdigit
+#include <stdlib.h>
 
+/**
+ * op_push - Pushes an element onto the stack
+ * @stack: Double pointer to the stack
+ * @line_number: Line number in the Monty byte code file
+ */
 void op_push(stack_t **stack, unsigned int line_number) {
-    /* Existing code */
+    char *arg = strtok(NULL, " \t\n");
 
-    if (!arg) {
-        fprintf(stderr, "L%d: usage: push integer\n", line_number);
+    if (!arg || !is_numeric(arg)) {
+        fprintf(stderr, "L%u: usage: push integer\n", line_number);
         exit(EXIT_FAILURE);
     }
 
-    /* Check if arg is a valid integer */
-    for (size_t i = 0; i < strlen(arg); i++) {
-        if (!isdigit(arg[i]) && !(i == 0 && arg[i] == '-')) {
-            fprintf(stderr, "L%d: usage: push integer\n", line_number);
-            exit(EXIT_FAILURE);
-        }
+    push_stack(stack, atoi(arg));
+}
+
+/**
+ * is_numeric - Checks if a string is a numeric value
+ * @str: String to check
+ *
+ * Return: 1 if numeric, 0 otherwise
+ */
+int is_numeric(char *str) {
+    if (!str || *str == '\0' || *str == '\n')
+        return 0;
+
+    int i = 0;
+    if (str[0] == '-' || str[0] == '+')
+        i++;
+
+    for (; str[i] != '\0' && str[i] != '\n'; i++) {
+        if (str[i] < '0' || str[i] > '9')
+            return 0;
     }
 
-    int value = atoi(arg);
-    /* Rest of your code */
+    return 1;
 }
