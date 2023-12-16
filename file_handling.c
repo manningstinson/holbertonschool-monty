0e8cdd4e6e1240f8481a74adc673524395eb042b
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-16 10:09:41 -0600

    Update file_handling.c

--- file_handling.c
+++ file_handling.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 /**
  * read_file - Reads the content of a file and executes Monty byte codes.
  * @filename: Name of the file to read
@@ -9,13 +10,13 @@
  *
  * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
  */
-int read_file(char *filename, stack_t **stack)
-{
+int read_file(char *filename, stack_t **stack) {
+    printf("Attempting to open file: %s\n", filename);  // Debugging print
+
     FILE *file = fopen(filename, "r");
 
-    if (!file)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", filename);
+    if (!file) {
+        perror("Error opening file");  // Print detailed error message
         return EXIT_FAILURE;
     }
 
@@ -23,15 +24,12 @@ int read_file(char *filename, stack_t **stack)
     size_t len = 0;
     unsigned int line_number = 0;
 
-    while (getline(&line, &len, file) != -1)
-    {
+    while (getline(&line, &len, file) != -1) {
         line_number++;
-        /* Remove newline character at the end of the line */
         size_t line_len = strlen(line);
         if (line_len > 0 && line[line_len - 1] == '\n')
             line[line_len - 1] = '\0';
 
-        /* Process the line and execute Monty byte codes */
         opcode_dispatcher(line, stack, line_number);
     }
 
