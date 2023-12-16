7731b021771723b0647f63e0428a534142a44a62
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-16 09:09:03 -0600

    Update push.c

--- push.c
+++ push.c
@@ -4,10 +4,17 @@
 #include "monty.h"
 #include <stdio.h>
 
-void push_stack(stack_t **stack, int value) {
+/**
+ * push_stack - Pushes a new node with a given value onto the stack.
+ * @stack: Double pointer to the stack
+ * @value: Value to be pushed onto the stack
+ */
+void push_stack(stack_t **stack, int value)
+{
     stack_t *new_node = malloc(sizeof(stack_t));
 
-    if (!new_node) {
+    if (!new_node)
+    {
         fprintf(stderr, "Error: malloc failed\n");
         cleanup(*stack);
         exit(EXIT_FAILURE);
@@ -23,11 +30,19 @@ void push_stack(stack_t **stack, int value) {
     *stack = new_node;
 }
 
-int is_numeric(const char *str) {
+/**
+ * is_numeric - Checks if a given string represents a numeric value.
+ * @str: String to be checked
+ *
+ * Return: 1 if numeric, 0 otherwise
+ */
+int is_numeric(const char *str)
+{
     if (!str || (*str == '-' && *(str + 1) == '\0'))
         return 0;
 
-    while (*str) {
+    while (*str)
+    {
         if (!isdigit(*str))
             return 0;
         str++;
@@ -36,10 +51,17 @@ int is_numeric(const char *str) {
     return 1;
 }
 
-void op_push(stack_t **stack, unsigned int line_number) {
+/**
+ * op_push - Executes the push operation.
+ * @stack: Double pointer to the stack
+ * @line_number: Line number of the Monty byte code file
+ */
+void op_push(stack_t **stack, unsigned int line_number)
+{
     char *arg = strtok(NULL, " \t\n");
 
-    if (!arg || !is_numeric(arg)) {
+    if (!arg || !is_numeric(arg))
+    {
         fprintf(stderr, "L%d: usage: push integer\n", line_number);
         cleanup(*stack);
         exit(EXIT_FAILURE);
