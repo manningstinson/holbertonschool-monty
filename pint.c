6ae357eb5c91d40da4fddba12ff591eb2efd12ee
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-16 10:42:57 -0600

    Update pint.c

--- pint.c
+++ pint.c
@@ -1,11 +1,10 @@
 #include "monty.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * op_pint - Prints the value at the top of the stack
- * @stack: Double pointer to the stack
- * @line_number: Line number in the Monty byte code file
+ * op_pint - Prints the value at the top of the stack.
+ * @stack: Double pointer to the stack.
+ * @line_number: Line number in the Monty byte code file.
  */
 void op_pint(stack_t **stack, unsigned int line_number) {
     if (!stack || !(*stack)) {
@@ -13,6 +12,5 @@ void op_pint(stack_t **stack, unsigned int line_number) {
         exit(EXIT_FAILURE);
     }
 
-    printf("Debug: Popping value from stack: %d\n", (*stack)->n);
     printf("%d\n", (*stack)->n);
 }
