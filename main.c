9330ad39a399230606e651cba8f521ab04355e9c
Manning Stinson <manning@manningstinson.com>
2023-12-18 08:48:07 -0800

    added betty styles to main.c

--- main.c
+++ main.c
@@ -5,8 +5,9 @@
 /**
  * cleanup_wrapper - Wrapper function for atexit with the correct signature
  */
-void cleanup_wrapper(void) {
-    cleanup(NULL);  // Passing NULL or any appropriate argument
+void cleanup_wrapper(void)
+{
+	cleanup(NULL);
 }
 
 /**
@@ -16,24 +17,27 @@ void cleanup_wrapper(void) {
  *
  * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
  */
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "USAGE: monty file\n");
-        return EXIT_FAILURE;
-    }
 
-    char *filename = argv[1];
-    stack_t *stack = NULL;
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		return (EXIT_FAILURE);
+	}
 
-    if (read_file(filename, &stack) == EXIT_FAILURE) {
-        fprintf(stderr, "Error: Can't open file %s\n", filename);
-        cleanup(stack);  // Cleanup on failure
-        return EXIT_FAILURE;
-    }
+	char *filename = argv[1];
 
-    atexit(cleanup_wrapper);
+	stack_t *stack = NULL;
 
-    cleanup(stack);  // Cleanup before exiting
+	if (read_file(filename, &stack) == EXIT_FAILURE) 
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", filename);
+		cleanup(stack);
+		return (EXIT_FAILURE);
+	}
+	atexit(cleanup_wrapper);
+	cleanup(stack);
 
-    return EXIT_SUCCESS;
+	return (EXIT_SUCCESS);
 }
